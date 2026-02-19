c209865369eecee991a0bd27460cf4d265fc163d
mdabdullahm <mdabdullahmorol38@gmail.com>
2026-02-19 14:43:26 +0600

    update code

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
            style={{ backgroundImage: 'linear-gradient(#10b981 1px, transparent 1px), linear-gradient(90deg, #10b981 1px, transparent 1px)', backgroundSize: '40px 40px' }}>
       </div>
 
-      <div className="max-w-[1440px] mx-auto px-6 lg:px-20 relative z-10">
+      <div className="max-w-full mx-auto px-6 lg:px-20 relative z-10">
         
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
           
